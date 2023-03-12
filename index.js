54e644ff4f3a6e458dee86e225ee24f748af4936
Andrea Zavatta <zavattaandrea@gmail.com>
2023-03-12 12:09:03 +0100

    add projects to site

--- index.js
+++ index.js
@@ -9,4 +9,18 @@ window.onload = function() {
             document.querySelector(".categories").classList.toggle("is-active");
         });
     });
+    addSmoothScrollTransition();
+}
+
+function addSmoothScrollTransition(){
+    var links = document.querySelectorAll('a[href^="#"]');
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            var target = document.querySelector(this.getAttribute('href'));
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    }
 }
