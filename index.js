64f10153ac7e97f63fc1ef3fe2c79e9f6c115c52
Andrea Zavatta <zavattaandrea@gmail.com>
2023-03-12 10:57:14 +0100

    create presentation page

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 window.onload = function() {
     document.querySelector(".hamburger").addEventListener("click", function() {
         document.querySelector(".hamburger").classList.toggle("is-active");
-        if(document.querySelector(".hamburger").classList.contains("is-active")) {
-            document.querySelector(".categories").classList.add("is-active");
-        } else {
-            document.querySelector(".categories").classList.remove("is-active");
-        }
+        document.querySelector(".categories").classList.toggle("is-active");
+    });
+    document.querySelectorAll(".categories a").forEach(function(el) {
+        el.addEventListener("click", function() {
+            document.querySelector(".hamburger").classList.toggle("is-active");
+            document.querySelector(".categories").classList.toggle("is-active");
+        });
     });
 }
