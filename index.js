745929b6079921068f622fac592c507696da2142
Andrea Zavatta <zavattaandrea@gmail.com>
2023-03-14 09:51:53 +0100

    possibility to switch between theme

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let mode = 0;
 window.onload = function() {
     document.querySelector(".hamburger").addEventListener("click", function() {
         document.querySelector(".hamburger").classList.toggle("is-active");
@@ -9,7 +10,50 @@ window.onload = function() {
             document.querySelector(".categories").classList.toggle("is-active");
         });
     });
+
+    document.querySelector(".mod").addEventListener("click", function() {
+        changeMode();
+    });
+
     addSmoothScrollTransition();
+    //document.getElementById("newsTodayText").innerText = "ciao";
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    $(function() {
+        $.get("http://numbersapi.com/"+mm+"/"+dd+"/date", function(data) {
+            $("#newsTodayText").text(data);
+        });
+    });
+}
+
+function changeMode(){
+    if(mode == 0){
+        // light mode
+        document.querySelectorAll(".flex-cards img").forEach(function(el) {
+            el.style.filter = "invert(90%)";
+        });
+        document.querySelector("iframe").style.filter = "none";
+        document.querySelector(".mod img").style.filter = "invert(90%)";
+        document.documentElement.style.setProperty("--back-color", "lightgray");
+        document.documentElement.style.setProperty("--nav-color", "white");
+        document.documentElement.style.setProperty("--text-color", "black");
+        document.documentElement.style.setProperty("--hamburger-color", "black");
+        mode = 1;
+    }else{
+        // dark mode
+        document.querySelector(".mod img").style.filter = "none";
+        document.querySelectorAll(".flex-cards img").forEach(function(el) {
+            el.style.filter = "none";
+        });
+        document.querySelector("iframe").style.filter = "invert(90%)";
+        document.documentElement.style.setProperty("--back-color", "#0A0C10");
+        document.documentElement.style.setProperty("--nav-color", "#272B33");
+        document.documentElement.style.setProperty("--text-color", "white");
+        document.documentElement.style.setProperty("--hamburger-color", "white");
+        mode = 0;
+    }
 }
 
 function initMap() {
